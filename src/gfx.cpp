99a42047fc538ba3d0c749d2e1bf3a3d1cc5e4d3
rbrugo <brugo.riccardo@gmail.com>
2020-03-14 02:04:41 +0100

    Cleanup

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -52,55 +52,21 @@ void display(
     auto rescale = [scale_coeff](auto const & _1) { return scale_coeff * _1; };
 
     // Build a "circle" to be displayed
-    auto to_circle = [&renderer, w, h](brun::position const & pos, SDLpp::color const color, float const radius) {
+    auto to_circle = [&renderer, w, h](brun::position const & pos, SDLpp::color const color, float const radius)
+    {
         auto const position = SDLpp::point2d(pos[0].count() + w/2, pos[1].count() + h/2);
         return SDLpp::paint::circle{renderer, position, radius, color};
     };
 
-    /**
-    auto transform_first = [](auto && f) { // Utility that transform only the first component of a triple
-        return rvw::transform([f=std::forward<decltype(f)>(f)](auto && t) mutable {
-            auto && [first, second, third] = t;
-            return std::tuple{
-                f(std::forward<decltype(first)>(first)),
-                std::forward<decltype(second)>(second),
-                std::forward<decltype(third)>(third)
-            };
-        });
-    };
-
-    auto filter_first = [](auto && f) { // Utility that apply a filter to the first memeber of a tuple
-        return rvw::filter([f=std::forward<decltype(f)>(f)](auto const & t) mutable {
-            auto const & first = std::get<0>(t);
-            return f(first);
-        });
-    }; */
-
-    // Create a list of triples with (position, color, px_radius); transform the first member in the displacement
-    //  relative to the COM, rescale it to fit in the screen if the magnitude is less than the max visual radius,
-    //  throw away objects which are too far away, then transform the triples into graphic circles
+    // We are going to choose which objects to draw.
+    // At first we will compute the displacement of every object from the origin, and rescale it so it
+    //  will be in [0, 1] if it is contained into the screen, (1, +∞) otherwise.
+    // Then we will discard every object whose rescaled displacement is greater than 1 and compute
+    //  the graphics to display (the circle and the motion trail) for every survived object
     auto entities  = registry.view<brun::position, SDLpp::color, brun::px_radius>();
-
-    /**
-    auto positions = entities
-                   | rvw::transform([&registry](auto const entt) { return registry.get<brun::position>(entt); });
-    auto colors    = entities
-                   | rvw::transform([&registry](auto const entt) { return registry.get<SDLpp::color>(entt); });
-    auto radii     = entities
-                   | rvw::transform([&registry](auto const entt) { return registry.get<brun::px_radius>(entt); });
-
-    auto circles = rvw::zip(positions, colors, radii)
-                  | transform_first(compute_displacement)
-                  | transform_first(rescale)
-                  | filter_first([k=std::max(w,h)*0.5](auto const & _1) { return brun::norm(_1) < k; })
-                  | rvw::transform([&to_circle](auto const & t) {
-                      auto const & [pos, col, rad] = t; return to_circle(pos, col, rad);
-                  })
-                  ;*/
-
     auto const k = std::max(w, h) * 0.5;
     auto circles = std::vector<SDLpp::paint::circle>(); circles.reserve(registry.size());
-    auto lines   = std::vector<SDLpp::paint::line>();   lines.reserve(registry.view<brun::trail>().size());
+    auto lines   = std::vector<SDLpp::paint::line  >(); lines.reserve(registry.view<brun::trail>().size());
     for (auto const entt : entities) {
         auto const pos = registry.get<brun::position>(entt);
         auto const displacement = compute_displacement(pos);
@@ -110,7 +76,6 @@ void display(
         }
 
         auto const [color, rad] = registry.get<SDLpp::color, brun::px_radius>(entt);
-        /* auto const x */
         auto const circle = to_circle(rescaled, color, rad);
         circles.push_back(circle);
 
@@ -121,7 +86,6 @@ void display(
         auto const [r, g, b, a] = color;
         auto const trail = registry.get<brun::trail>(entt);
         auto scaled_trail = trail
-                          /* | rvw::unique */
                           | rvw::transform(compute_displacement)
                           | rvw::transform(rescale)
                           ;
@@ -138,37 +102,20 @@ void display(
             auto const y1 = double(p1[1] + h/2);
             return SDLpp::paint::line{renderer, SDLpp::point2d{x0, y0}, {x1, y1}, {r, g, b, alpha}};
         };
-        /* auto trail_blended = rvw::zip_with( */
-            /* rvw::zip(scaled_trail, rvw::tail(scaled_trail)), alpha, to_line */
-        /* ); */
+
         for (auto const [pts, alpha] : rvw::zip(rvw::zip(scaled_trail, rvw::tail(scaled_trail)), alpha)) {
             lines.push_back(to_line(pts, alpha));
         }
-
-        /* ranges::actions::push_back(lines, trail_blended | rvw::move); */
     }
 
-
-    // TODO FIXME
-    // elimina il blending della scia e media il colore con il nero
-    // altrimenti reimplementare velocemente bresenham
-#warning LEGGI QUI RIGA 154 DI GFX.CPP
-    auto line_draw = [&renderer](auto const line) {
-        /* renderer.set_draw_color(SDLpp::colors::black); */
-        /* auto const bg = SDLpp::paint::line{ */
-            /* renderer, line.position(), line.position(), line.thickness(), SDLpp::colors::black */
-        /* }; */
-        /* bg.display(); */
-        line.display();
-    };
     // Make the screen black
     renderer.blend_mode(SDLpp::flag::blend_mode::none);
     renderer.set_draw_color(SDLpp::colors::black);
     renderer.clear();
     {
         auto lock = std::scoped_lock{mtx};  // Lock the data (for safety reasons in multithreading)
-        ranges::for_each(lines,   line_draw);//&SDLpp::paint::line  ::display);
-        ranges::for_each(circles, &SDLpp::paint::circle::display); // Draw circles on the "canvas"
+        ranges::for_each(lines,   &SDLpp::paint::line  ::display); // Draw motion trail first,
+        ranges::for_each(circles, &SDLpp::paint::circle::display); //  then circles, on the "canvas"
     }
     renderer.present(); // Display the canvas
 }
