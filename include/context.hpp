5f54ddff64c0063abc9550eacb8b930831e9685d
rbrugo <brugo.riccardo@gmail.com>
2020-03-22 00:57:03 +0100

    Added status flag; refactored

--- include/context.hpp
+++ include/context.hpp
@@ -8,6 +8,7 @@
 #ifndef CAMERA_HPP
 #define CAMERA_HPP
 
+#include <atomic>
 #include <variant>
 #include <shared_mutex>
 
@@ -18,32 +19,43 @@
 namespace brun
 {
 
+/// Represent the camera policy
 namespace follow
 {
-struct com    { brun::position offset; };
-struct nothing{ brun::position last; };
-struct target {
-    target(entt::entity const tg) : id{tg} {}
+struct com    { brun::position offset; }; /// Follows the center of mass
+struct nothing{ brun::position offset; }; /// Stay in place
+struct target                             /// Follow a target
+{
+    constexpr target(entt::entity const tg) : id{tg} {}
     entt::entity id;
     brun::position offset;
 };
 } // namespace follow
 using follow_t = std::variant<follow::com, follow::nothing, follow::target>;
 
+/// Represent the status of the simulation
+enum class status : int8_t
+{
+    starting,   // Simulation thread is preparing
+    running,    // Simulation thread has started, now other thread can follow
+    stopped     // All threads have to stop
+};
+
 struct context
 {
+    std::atomic<brun::status> status = status::starting;
     entt::registry reg;
     follow_t follow;
 private:
-    mutable std::shared_mutex reg_mtx;
+    mutable std::shared_mutex ctx_mtx;
 
-    inline void lock()     const noexcept { reg_mtx.lock();   }
-    inline bool try_lock() const noexcept { return reg_mtx.try_lock(); }
-    inline void unlock()   const noexcept { reg_mtx.unlock(); }
+    inline void lock()     const noexcept { ctx_mtx.lock();   }
+    inline bool try_lock() const noexcept { return ctx_mtx.try_lock(); }
+    inline void unlock()   const noexcept { ctx_mtx.unlock(); }
 
-    inline void lock_shared()     const noexcept { reg_mtx.lock_shared(); }
-    inline bool try_lock_shared() const noexcept { return reg_mtx.try_lock_shared(); }
-    inline void unlock_shared()   const noexcept { return reg_mtx.unlock_shared(); }
+    inline void lock_shared()     const noexcept { ctx_mtx.lock_shared(); }
+    inline bool try_lock_shared() const noexcept { return ctx_mtx.try_lock_shared(); }
+    inline void unlock_shared()   const noexcept { return ctx_mtx.unlock_shared(); }
 };
 
 } // namespace brun
